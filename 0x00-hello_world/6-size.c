bc8fa48ee23bab1c2fdae3551e762fd8a2d19a76
waireghie <victorwairegi@gmail.com>
2023-09-07 11:13:27 -0700

    size is not grandeur I tello you

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * return: if 0 success
+ */
+int main(0)
+{
+printf("size of char: %lu bytes\n",
+sizeof(char));
+printf("size of int: %lu bytes\n",
+sizeof(int));
+printf("size of float: %lu bytes\n",
+sizeof(float));
+printf("size of long: %lu bytes\n",
+sizeof(long));
+printf("size of long long: %lu bytes\n",
+sizeof(long long));
+return (0);
+}
