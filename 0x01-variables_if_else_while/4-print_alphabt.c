fbe0f072a3e1cb92cae0aecdde925bb8c2cd8ddf
waireghie <victorwairegi@gmail.com>
2023-09-08 14:14:37 -0700

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ * Return: when 0, means success
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+			}
+	putchar('\n');
+	return (0);
+}
