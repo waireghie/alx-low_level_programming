8e044d0d79def0636429a512ad7257a5f33a201c
waireghie <victorwairegi@gmail.com>
2023-09-08 15:37:16 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 value is right
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 0; i <= 9; i++)
+        {
+                if (i > 0)
+		{
+                        putchar( ',');
+                        putchar('');
+		}
+		putchar('o' + i);
+        }
+        putchar('\n');
+        return (0);
+}
+
