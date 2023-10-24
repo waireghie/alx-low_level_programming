a15bba5482cf36c7d6a7a0cf8cf59d7ed221aed6
vikenya <victorwairegi@gmail.com>
2023-10-24 12:12:21 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef  OBJECCT_LIKE_MACRO_
-#define  OBJECCT_LIKE_MACRO_
-
-#define SIZE 1024
-
-#endif
--- 0x0D-preprocessor/0.
+++
@@ -1,6 +0,0 @@
-#ifdef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-#define SIZE 1024
-
-#endif
